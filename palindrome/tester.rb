4a55f9478e9cc236a01ab13150443b9f47f37de9
Jeff Bumgardner <bumgardner.jeffrey@gmail.com>
2017-01-17 21:42:20 -0500

    palindrome exercise complete.

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -1,13 +1,27 @@
 class Tester
+  # only for letters with no caps, spaces, or symbols
   class T1
-    def palindrome?(string)
+    def palindrome(string)
       # first implementation
+      if string == string.reverse
+        print "it worked"
+      else
+        print "It did not work"
+      end
     end
   end
 
+# checks for different types of symbols and numbers
   class T2
-    def palindrome?(string)
-      # second implementation
+    def palindrome(string)
+      # first implementation
+      argument = string.gsub!(/[.!'', ]/, '')
+      lowercase = argument.downcase
+      reversed = lowercase.reverse
+      if lowercase == reversed
+        print "it worked"
+      else
+        print "It did not work"
+      end
     end
-  end
 end
